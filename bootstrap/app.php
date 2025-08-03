ed8b4d8ec6ae7d897a739990decffb4adc84c8e2
jhleeon <jhleon288@gmail.com>
2025-08-03 14:05:19 +0600

    feat: Route protection add

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\UserMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,19 +14,22 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
         then: function() {
-            Route::middleware(['auth:sanctum'])
+            Route::middleware(['auth:sanctum', 'admin'])
             ->prefix('api/v1/')
             ->name('admin.')
             ->group(base_path('routes/api/v1/admin.php'));
 
-            Route::middleware(['auth:sanctum'])
+            Route::middleware(['auth:sanctum', 'user'])
             ->prefix('api/v1/')
             ->name('user.')
             ->group(base_path('routes/api/v1/user.php'));
         }
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+         $middleware->alias([
+            'admin' => AdminMiddleware::class,
+            'user' => UserMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
